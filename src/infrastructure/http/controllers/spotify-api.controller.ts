2033926db78f060e8653a4069de1459f0c8bfc87
Carlo Santos <jcms121899@gmail.com>
2024-01-17 13:55:53 +0800

    Added spotify api service and controller (#3)

--- src/infrastructure/http/controllers/spotify-api.controller.ts
+++ src/infrastructure/http/controllers/spotify-api.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { SpotifyApiService } from '@infrastructure/services/spotify-api.service';
+import { Controller, Get, Body, Post, Param } from '@nestjs/common';
 
 @Controller('spotify')
-export class SpotifyApiController {}
+export class SpotifyApiController {
+  constructor(private readonly spotifyApiService: SpotifyApiService) {}
+
+  @Get('/jb')
+  async getJB() {
+    return await this.spotifyApiService.getJB();
+  }
+
+  @Get('/:artistId')
+  async getArtist(@Param('artistId') artistId: string) {
+    return await this.spotifyApiService.getArtist(artistId);
+  }
+}
--- src/infrastructure/modules/spotify-api.module.ts
+++ src/infrastructure/modules/spotify-api.module.ts
@@ -2,10 +2,12 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SpotifyApiController } from '../http/controllers/spotify-api.controller';
 import { SpotifyApiService } from '../services/spotify-api.service';
+import { SpotifyAuthApiModule } from './spotify-auth-api.module';
+import { SpotifyAuthApiService } from '@infrastructure/services/spotify-auth-api.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, SpotifyAuthApiModule],
   controllers: [SpotifyApiController],
-  providers: [SpotifyApiService],
+  providers: [SpotifyApiService, SpotifyAuthApiService],
 })
 export class SpotifyApiModule {}
